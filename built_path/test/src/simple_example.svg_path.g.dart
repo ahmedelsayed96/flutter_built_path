12447b7531e1c567b6b1a8b6d62269c188bbdf6e
ahmido <ahmed.elsayed.ali20@gmail.com>
2021-04-15 16:04:56 +0200

    //

--- built_path/test/src/simple_example.svg_path.g.dart
+++ built_path/test/src/simple_example.svg_path.g.dart
@@ -1,35 +1,35 @@
-// ignore_for_file: non_constant_identifier_names
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'simple_example.dart';
-
-// **************************************************************************
-// SvgPathGenerator
-// **************************************************************************
-
-Path __$path;
-Path get _$path =>
-    __$path ??
-    (__$path = new Path()
-      ..moveTo(10.0, 10.0)
-      ..lineTo(20.0, 20.0)
-      ..close());
-Path __$star;
-Path get _$star =>
-    __$star ??
-    (__$star = new Path()
-      ..moveTo(250.0, 75.0)
-      ..lineTo(323.0, 301.0)
-      ..lineTo(131.0, 161.0)
-      ..lineTo(369.0, 161.0)
-      ..lineTo(177.0, 301.0)
-      ..close()
-      ..fillType = PathFillType.evenOdd);
-Path __$Paths_anotherPath;
-Path get _$Paths_anotherPath =>
-    __$Paths_anotherPath ??
-    (__$Paths_anotherPath = new Path()
-      ..moveTo(0.0, 0.0)
-      ..lineTo(20.0, 10.0)
-      ..cubicTo(10.0, 10.0, 15.0, 15.0, 20.0, 10.0)
-      ..close());
+// // ignore_for_file: non_constant_identifier_names
+// // GENERATED CODE - DO NOT MODIFY BY HAND
+//
+// part of 'simple_example.dart';
+//
+// // **************************************************************************
+// // SvgPathGenerator
+// // **************************************************************************
+//
+// Path __$path;
+// Path get _$path =>
+//     __$path ??
+//     (__$path = new Path()
+//       ..moveTo(10.0, 10.0)
+//       ..lineTo(20.0, 20.0)
+//       ..close());
+// Path __$star;
+// Path get _$star =>
+//     __$star ??
+//     (__$star = new Path()
+//       ..moveTo(250.0, 75.0)
+//       ..lineTo(323.0, 301.0)
+//       ..lineTo(131.0, 161.0)
+//       ..lineTo(369.0, 161.0)
+//       ..lineTo(177.0, 301.0)
+//       ..close()
+//       ..fillType = PathFillType.evenOdd);
+// Path __$Paths_anotherPath;
+// Path get _$Paths_anotherPath =>
+//     __$Paths_anotherPath ??
+//     (__$Paths_anotherPath = new Path()
+//       ..moveTo(0.0, 0.0)
+//       ..lineTo(20.0, 10.0)
+//       ..cubicTo(10.0, 10.0, 15.0, 15.0, 20.0, 10.0)
+//       ..close());
